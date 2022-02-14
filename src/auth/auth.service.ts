02f58f4152808661e81cd3d5fe0b24291ba4ba59
snippy <snippymr@yandex.ru>
2022-02-14 18:39:18 +0300

    sueta

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -5,6 +5,7 @@ import * as bcrypt from 'bcryptjs';
 import { UserDto, AuthUserDto } from '../users/dto/user.dto';
 import { UsersService } from '../users/users.service';
 import { User } from '../users/user.model';
+import { validateEmail } from '../utils/functionHelper'
 
 @Injectable()
 export class AuthService {
@@ -21,6 +22,49 @@ export class AuthService {
 
   async registration(userDto: UserDto) {
     const candidate = await this.usersService.getUserByEmail(userDto.email);
+
+    if (!userDto.email) {
+      throw new HttpException(
+        'Введите email',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    if (!userDto.password) {
+      throw new HttpException(
+        'Введите password',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    if (!userDto.name) {
+      throw new HttpException(
+        'Введите login',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    if (userDto.password.length < 7) {
+      throw new HttpException(
+        'Минимум 7 символов для password',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+
+    if (userDto.email.length > 50 ||  userDto.password.length > 50 || userDto.name.length > 50) {
+      throw new HttpException(
+        'Максимум 50 символов',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    if(!validateEmail(userDto.email)){
+      throw new HttpException(
+        'Неверный email',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
 
     if (candidate) {
       throw new HttpException(
@@ -43,11 +87,40 @@ export class AuthService {
 
     return {
       token: this.jwtService.sign(payload),
+      login: user.name,
     };
   }
 
   private async validateUser(userDto: AuthUserDto) {
     const user = await this.usersService.getUserByEmail(userDto.email);
+
+    if (!userDto.email) {
+      throw new HttpException(
+        'Введите email',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    if (!userDto.password) {
+      throw new HttpException(
+        'Введите password',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    if (userDto.email.length > 50 ||  userDto.password.length > 50) {
+      throw new HttpException(
+        'Максимум 50 символов',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    if(!validateEmail(userDto.email)){
+      throw new HttpException(
+        'Неверный email',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
 
     if (!user) {
       throw new HttpException(
@@ -55,6 +128,8 @@ export class AuthService {
         HttpStatus.BAD_REQUEST,
       );
     }
+
+
 
     const passwordEquals = await bcrypt.compare(
       userDto.password,
--- src/utils/functionHelper.ts
+++ src/utils/functionHelper.ts
@@ -167,3 +167,10 @@ export function generateTasks(): TasksDto {
 
   return taskArray[randomTask];
 }
+
+export function validateEmail(email) {
+  const re =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
+
